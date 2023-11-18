b0f6fad9014dc7d15119c5ff49b4d36e532330ba
ruofan <px732amgo81sp8@gmail.com>
2023-11-18 17:11:19 +0800

    update

--- backend/transactions/models.py
+++ backend/transactions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Transaction(models.Model):
     amount = models.IntegerField()
     user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
-    category = models.ForeignKey("category.Category", on_delete=models.CASCADE)
+    category = models.ForeignKey("categories.Category", on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
