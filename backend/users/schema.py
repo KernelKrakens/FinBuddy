f4a9c0fc30b3c844e2f60a1f2c707f6f47c290df
Kalle Chen <kallechen.tw@gmail.com>
2023-11-19 20:01:03 +0800

    feat: add google auth

--- backend/users/schema.py
+++ backend/users/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 import graphql_jwt
+from graphql_jwt.shortcuts import get_token
+import requests
 
 from .models import CustomUser
 
@@ -11,7 +13,12 @@ class UserType(DjangoObjectType):
         model = CustomUser
 
 
-# RegisterUser mutation
+class LimitedUserType(DjangoObjectType):
+    class Meta:
+        model = CustomUser
+        fields = ("id", "email")  # example of limited fields
+
+
 class RegisterUser(graphene.Mutation):
     class Arguments:
         email = graphene.String(required=True)
@@ -25,9 +32,44 @@ class RegisterUser(graphene.Mutation):
         return RegisterUser(user=user)
 
 
-# Query class
+class GoogleLogin(graphene.Mutation):
+    class Arguments:
+        id_token = graphene.String(required=True)
+
+    jwt_token = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, id_token):
+        google_token_info_url = (
+            "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + id_token
+        )
+
+        try:
+            response = requests.get(google_token_info_url)
+            token_info = response.json()
+
+            if not token_info.get("error"):
+                if token_info["aud"] == "your-google-client-id":
+                    user_email = token_info["email"]
+
+                    user, created = CustomUser.objects.get_or_create(email=user_email)
+                    if created:
+                        user.set_password(CustomUser.objects.make_random_password())
+                        user.save()
+
+                    jwt_token = get_token(user)
+
+                    return GoogleLogin(jwt_token=jwt_token)
+                else:
+                    raise Exception("Invalid audience/client ID")
+            else:
+                raise Exception("Invalid Google OAuth2 token")
+        except Exception as e:
+            raise Exception("Google authentication failed: " + str(e))
+
+
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
+    users = graphene.List(LimitedUserType)
     user_info = graphene.Field(UserType)
 
     def resolve_users(self, info):
