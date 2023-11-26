a1f634924af5fa052f0a9621f5f4005ed7218e42
ruofan <px732amgo81sp8@gmail.com>
2023-11-26 22:24:04 +0800

    CATEGORY_CHOICES

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-class Category(models.Model):
-    INCOME = "income"
-    OUTCOME = "outcome"
-    CATEGORY_CHOICES = [
-        (INCOME, "Income"),
-        (OUTCOME, "Outcome"),
-    ]
+CATEGORY_CHOICES = [
+    ("income", "Income"),
+    ("outcome", "Outcome"),
+]
 
+
+class Category(models.Model):
     category_type = models.CharField(max_length=250, choices=CATEGORY_CHOICES)
     name = models.CharField(max_length=250)
     created_user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
