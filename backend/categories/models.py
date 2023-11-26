297992397e1ca4093de737a2b2de8153c0d1272d
ruofan <px732amgo81sp8@gmail.com>
2023-11-26 22:09:16 +0800

    income/outcome

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_type = models.CharField(max_length=250)
+    INCOME = "income"
+    OUTCOME = "outcome"
+    CATEGORY_CHOICES = [
+        (INCOME, "Income"),
+        (OUTCOME, "Outcome"),
+    ]
+
+    category_type = models.CharField(max_length=250, choices=CATEGORY_CHOICES)
     name = models.CharField(max_length=250)
     created_user = models.ForeignKey("users.CustomUser", on_delete=models.CASCADE)
 
