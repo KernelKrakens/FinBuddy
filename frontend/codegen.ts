d37f0a1facd9d54f49ce8e0d7d811f5025fea992
Kalle Chen <kallechen.tw@gmail.com>
2024-02-18 16:31:01 +0800

    fix: compile failed in docker compose

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,7 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
+import { parsedEnv } from './src/lib/processEnv'
+
 const config: CodegenConfig = {
-  schema: 'http://localhost:8000/graphql/',
+  schema: `${parsedEnv.BACKEND_URI}/graphql/`,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
