d60744ed263af1031ae17bccbbaaae7a6aa60011
Kalle Chen <kallechen.tw@gmail.com>
2024-03-03 05:47:37 +0800

    fix:  backend domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["backend"]
+ALLOWED_HOSTS = ["backend", "localhost"]
 
 
 # Application definition
