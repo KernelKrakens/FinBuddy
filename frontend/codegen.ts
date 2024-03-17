fc0b7c900ad5606ccdea2e647737b20272b5e341
Kalle Chen <kallechen.tw@gmail.com>
2024-03-17 13:03:12 +0800

    fix: backend domain

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,9 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
-import { parsedEnv } from './src/lib/processEnv'
-
 const config: CodegenConfig = {
-  schema: `${parsedEnv.BACKEND_URI}/graphql/`,
+  schema: `http://backend:8000/graphql/`,
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
