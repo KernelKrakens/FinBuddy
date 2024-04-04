92fe20d2af7d39ece7a1e870b12af069c2f9a269
ruofan <px732amgo81sp8@gmail.com>
2024-04-04 14:44:11 +0800

    feat mutation for category and transaction

--- backend/categories/schema.py
+++ backend/categories/schema.py
@@ -1,6 +1,8 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Category
+from users.models import CustomUser
+from graphql import GraphQLError
 
 
 class CategoryType(DjangoObjectType):
@@ -16,4 +18,71 @@ class Query(graphene.ObjectType):
         return Category.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class AddCategory(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+        category_type = graphene.String(required=True)
+        created_user_id = graphene.ID(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, name, category_type, created_user_id):
+        try:
+            created_user = CustomUser.objects.get(pk=created_user_id)
+        except CustomUser.DoesNotExist:
+            raise GraphQLError("No user found with the provided ID")
+
+        category = Category(
+            name=name, category_type=category_type, created_user=created_user
+        )
+        category.save()
+        return AddCategory(category=category)
+
+
+class UpdateCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        category_type = graphene.String()
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, id, name=None, category_type=None):
+        try:
+            category = Category.objects.get(pk=id)
+        except Category.DoesNotExist:
+            raise GraphQLError("No category found with the provided ID")
+        if name is not None:
+            category.name = name
+        if category_type is not None:
+            category.category_type = category_type
+        category.save()
+        return UpdateCategory(category=category)
+
+
+class DeleteCategory(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        try:
+            category = Category.objects.get(pk=id)
+        except Category.DoesNotExist:
+            raise GraphQLError("No category found with the provided ID")
+
+        category.delete()
+        return DeleteCategory(success=True)
+
+
+class Mutation(graphene.ObjectType):
+    add_category = AddCategory.Field()
+    update_category = UpdateCategory.Field()
+    delete_category = DeleteCategory.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- backend/transactions/schema.py
+++ backend/transactions/schema.py
@@ -1,6 +1,10 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Transaction
+from users.models import CustomUser
+from categories.models import Category
 
 
 class TransactionType(DjangoObjectType):
@@ -16,4 +20,82 @@ class Query(graphene.ObjectType):
         return Transaction.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class AddTransaction(graphene.Mutation):
+    class Arguments:
+        amount = graphene.Int(required=True)
+        user_id = graphene.ID(required=True)
+        category_id = graphene.ID(required=True)
+        date = graphene.DateTime(required=True)
+
+    transaction = graphene.Field(TransactionType)
+
+    @classmethod
+    def mutate(cls, root, info, amount, user_id, category_id, date):
+        try:
+            user = CustomUser.objects.get(pk=user_id)
+            category = Category.objects.get(pk=category_id)
+        except ObjectDoesNotExist as e:
+            raise GraphQLError("An object does not exist with given ID: " + str(e))
+
+        transaction = Transaction(
+            amount=amount, user=user, category=category, date=date
+        )
+        transaction.save()
+        return AddTransaction(transaction=transaction)
+
+
+class UpdateTransaction(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        amount = graphene.Int()
+        category_id = graphene.ID()
+        date = graphene.DateTime()
+
+    transaction = graphene.Field(TransactionType)
+
+    @classmethod
+    def mutate(cls, root, info, id, amount=None, category_id=None, date=None):
+        try:
+            transaction = Transaction.objects.get(pk=id)
+        except Transaction.DoesNotExist:
+            raise GraphQLError("No transaction found with the provided ID")
+
+        if category_id is not None:
+            try:
+                transaction.category = Category.objects.get(pk=category_id)
+            except Category.DoesNotExist:
+                raise GraphQLError("No category found with the provided ID")
+
+        if amount is not None:
+            transaction.amount = amount
+        if date is not None:
+            transaction.date = date
+
+        transaction.save()
+        return UpdateTransaction(transaction=transaction)
+
+
+class DeleteTransaction(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+
+    success = graphene.Boolean()
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        try:
+            transaction = Transaction.objects.get(pk=id)
+        except Transaction.DoesNotExist:
+            raise GraphQLError("No transaction found with the provided ID")
+
+        transaction.delete()
+        return DeleteTransaction(success=True)
+
+
+class Mutation(graphene.ObjectType):
+    add_transaction = AddTransaction.Field()
+    update_transaction = UpdateTransaction.Field()
+    delete_transaction = DeleteTransaction.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
